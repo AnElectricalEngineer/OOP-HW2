08dafc293fa6d85574307dde277edbd8a51a3b1a
AnElectricalEngineer <47844685+AnElectricalEngineer@users.noreply.github.com>
2021-01-04 17:26:20 +0200

    added white box tests for Graph

--- src/homework2/GraphTests.java
+++ src/homework2/GraphTests.java
@@ -1,5 +1,8 @@
 package homework2;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+import homework2.GraphExceptions.*;
 
 /**
  * This class contains a set of test cases that can be used to test the graph
@@ -13,6 +16,76 @@ public class GraphTests extends ScriptFileTests {
 		super(testFile);
 	}
 
-	// TODO: add additional white box tests
+	// Defines white box tests for the Graph class
+
+	@Test(expected = NodeAlreadyExistsException.class)
+	public void addNodeTest() throws NodeAlreadyExistsException
+	{
+		Graph<WeightedNode> graph1 = new Graph<>("G1");
+
+		WeightedNode n1 = new WeightedNode("n1", 1);
+		WeightedNode n2 = new WeightedNode("n1", 1);
+
+		graph1.addNode(n1);
+		graph1.addNode(n2);
+	}
+
+	@Test(expected = NodeDoesNotExistException.class)
+	public void addEdgeTest1() throws NodeAlreadyExistsException,
+			EdgeAlreadyExistsException, NodeDoesNotExistException
+	{
+		Graph<WeightedNode> graph1 = new Graph<>("G1");
+
+		WeightedNode n1 = new WeightedNode("n1", 1);
+		WeightedNode n2 = new WeightedNode("n2", 2);
+
+		graph1.addNode(n1);
+		graph1.addEdge(n1, n2);
+	}
+
+	@Test(expected = NodeDoesNotExistException.class)
+	public void addEdgeTest2() throws NodeAlreadyExistsException,
+			EdgeAlreadyExistsException, NodeDoesNotExistException
+	{
+		Graph<WeightedNode> graph1 = new Graph<>("G1");
+
+		WeightedNode n1 = new WeightedNode("n1", 1);
+		WeightedNode n2 = new WeightedNode("n2", 2);
+		WeightedNode n3 = new WeightedNode("n3", 2);
+
+		graph1.addNode(n1);
+		graph1.addEdge(n2, n3);
+	}
+
+	@Test(expected = EdgeAlreadyExistsException.class)
+	public void addEdgeTest3() throws NodeAlreadyExistsException,
+			EdgeAlreadyExistsException, NodeDoesNotExistException
+	{
+		Graph<WeightedNode> graph1 = new Graph<>("G1");
+
+		WeightedNode n1 = new WeightedNode("n1", 1);
+		WeightedNode n2 = new WeightedNode("n2", 2);
+
+		graph1.addNode(n1);
+		graph1.addNode(n2);
+		graph1.addEdge(n1, n2);
+		graph1.addEdge(n1, n2);
+	}
+
+	@Test(expected = NodeDoesNotExistException.class)
+	public void getChildNodesTest() throws NodeAlreadyExistsException,
+			EdgeAlreadyExistsException, NodeDoesNotExistException
+	{
+		Graph<WeightedNode> graph1 = new Graph<>("G1");
+
+		WeightedNode n1 = new WeightedNode("n1", 1);
+		WeightedNode n2 = new WeightedNode("n2", 2);
+		WeightedNode n3 = new WeightedNode("n3", 2);
+
+		graph1.addNode(n1);
+		graph1.addNode(n2);
+		graph1.addEdge(n1, n2);
+		graph1.getChildNodes(n3);
+	}
 
 }
